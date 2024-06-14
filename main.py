dcbc3a2c64de7142013b5444acd1f846245178d4
ykoke <166777088+ykoke@users.noreply.github.com>
2024-06-14 13:35:16 +0900

    20240614_Ver5

--- main.py
+++ main.py
@@ -52,4 +52,4 @@ def index():
 
 @app.post("/present")
 async def new_naming(present):
-    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}
+    return {"response": f"サーバです。メリークリスマス！" {present} "ありがとう。お返しはキャンディーです。"}
