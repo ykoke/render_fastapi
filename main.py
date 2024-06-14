a24d5e527791c5e9f277f40adb067a904a6c0cf7
ykoke <166777088+ykoke@users.noreply.github.com>
2024-06-14 12:46:51 +0900

    20240614_Ver1

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from typing import Optional
 
 from fastapi import FastAPI
 
-<<<<<<< HEAD
+import random
+
 from fastapi.responses import HTMLResponse #インポート
 
 
@@ -16,8 +17,23 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
-<<<<<<< HEAD
 
+@app.get("/omikuji")
+def omikuji():
+    omikuji_list = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "吉",
+        "半吉",
+        "末吉",
+        "末小吉",
+        "凶",
+        "小凶",
+        "大凶"
+    ]
+    
+    return {"result" : omikuji_list[random.randrange(10)]}
 
 @app.get("/index")
 def index():
